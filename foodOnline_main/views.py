91da585d48ac0695c0a5b1c37c9d2f294c8d2040
Chandan <cksthebest@gmail.com>
2023-12-06 12:52:19 +0530

    Homepage and postgres setup

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
 def home(request):
-    return HttpResponse("This is Home")
+    return render(request, "home.html")
